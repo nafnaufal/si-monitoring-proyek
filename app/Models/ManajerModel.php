d2d1b33d82d221deb4715041ce914be1bfbf288a
nafnaufal <naufalanbial@gmail.com>
2022-10-19 11:03:29 +0700

    model edit

--- app/Models/ManajerModel.php
+++ app/Models/ManajerModel.php
@@ -6,14 +6,10 @@ use CodeIgniter\Model;
 
 class ManajerModel extends Model
 {
-    protected $DBGroup          = 'default';
     protected $table            = 'manajer';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
+    protected $primaryKey       = "username";
     protected $returnType       = "object";
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
+    protected $useTimestamps    = true;
     protected $allowedFields    = ['username', 'password', 'name', 'tgl_lahir', 'kelamin', 'alamat', 'email', 'no_telp'];
 
 }
