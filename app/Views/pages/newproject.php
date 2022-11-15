7c551f9bddf192a7e85703665356417e1430348d
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2022-11-16 00:32:12 +0700

    adding project details

--- app/Views/pages/newproject.php
+++ app/Views/pages/newproject.php
@@ -11,12 +11,10 @@
             <label for="desc">Deskripsi</label>
             <textarea name="desc" id="desc" class="form-control" cols="30" rows="10"></textarea>
             <label for="divisi">Divisi</label>
-            <select class="form-control" id="divisi" name="divisi">
-                <option value="none" selected disabled hidden>Pilih Divisi</option>
-                <?php foreach ($data as $dt) { ?>
-                    <option value="<?= $dt['id'] ?>"><?= $dt['name'] ?></option>
-                <?php } ?>
-            </select>
+            <select class="form-select ms-1" aria-label=".form-select-sm" name="divisi">
+                                                <option value="Man" selected>Man</option>
+                                                <option value="Woman">Woman</option>
+                                              </select>
             <label for="deadline">Deadline</label>
             <input type="date" class="form-control" id="deadline" name="deadline">
             <button type="submit" class="btn btn-primary mt-2">Insert</button>
